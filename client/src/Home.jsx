3f7495b310e1a0c8ec92c680d0614dd2838eb176
Ellina Shcherbaeva <s.ellina2020@gmail.com>
2023-02-05 21:21:08 +1100

    Moving puzzle

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,6 +1,47 @@
+import React from "react";
 import Puzzle from "./Puzzle";
+import { useEffect, useState } from "react";
+
+const speed = 10;
 
 export default function Home() {
+  const [position, setPosition] = useState({ x: 232, y: 154 });
+
+  useEffect(() => {
+    const keyHandler = (e) => {
+      if (e.key === "ArrowDown") {
+        setPosition((prevPosition) => {
+          return { ...prevPosition, y: prevPosition.y + speed };
+        });
+      }
+      if (e.key === "ArrowUp") {
+        setPosition((prevPosition) => {
+          return { ...prevPosition, y: prevPosition.y - speed };
+        });
+      }
+
+      if (e.key === "ArrowLeft") {
+        setPosition((prevPosition) => {
+          return { ...prevPosition, x: prevPosition.x - speed };
+        });
+      }
+
+      if (e.key === "ArrowRight") {
+        setPosition((prevPosition) => {
+          return { ...prevPosition, x: prevPosition.x + speed };
+        });
+      }
+    };
+
+    document.addEventListener("keydown", keyHandler);
+    //Remove the eventListener on the Main Page when we unmount,
+    //To prevent double Listening when we remount
+    //We remount every time when we navigate to the Home Page
+    return () => {
+      document.removeEventListener("keydown", keyHandler);
+    };
+  }, []);
+
   return (
     <div className="flex">
       <div className="bg-red-500 grow">
@@ -14,9 +55,12 @@ export default function Home() {
             color="#92d3f5"
             className="absolute top-[124px] left-[16px]"
           ></Puzzle>
+          <Puzzle
+            color="#92d3f5"
+            className="absolute"
+            style={{ left: position.x, top: position.y }}
+          ></Puzzle>
         </div>
-
-        {/* <Puzzle color="#92d3f5"></Puzzle> */}
       </div>
       <div className="bg-green-500 w-64">
         <ul>
