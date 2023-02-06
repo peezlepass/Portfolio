02324abe900991b0c9776771c911aeb70bf690ca
Ellina Shcherbaeva <s.ellina2020@gmail.com>
2023-02-06 20:43:45 +1100

    Todo style with TheNounProject

--- client/src/Checkbox.jsx
+++ client/src/Checkbox.jsx
@@ -12,7 +12,23 @@ export default function Checkbox() {
         enabled ? "bg-blue-600" : "bg-gray-200"
       } relative inline-flex h-7 w-7 items-center rounded-lg`}
     >
-      <span className="sr-only">Enable notifications</span>
+      {enabled ? (
+        <svg
+          className="mt-1"
+          width="25px"
+          height="25px"
+          version="1.1"
+          viewBox="0 0 700 700"
+        >
+          <path
+            fill="white"
+            d="m292.88 482.72c-15.68 0-30.801-6.1602-42-17.359l-149.52-149.52c-22.961-22.961-22.961-60.48 0-83.441 22.961-22.961 60.48-22.961 83.441 0l106.4 106.4 221.76-241.92c21.84-24.078 59.359-25.762 83.441-3.3594 24.078 21.84 25.762 59.359 3.3594 83.441l-263.2 286.72c-10.641 11.762-26.32 19.039-42.559 19.039h-1.1211z"
+          />
+        </svg>
+      ) : null}
+      <span className="sr-only">Toggle complete</span>
     </Switch>
   );
 }
+
+Checkbox.attributions = `tick by Adrien Coquet from <a href="https://thenounproject.com/browse/icons/term/tick/" target="_blank" title="tick Icons">Noun Project</a>`;
--- client/src/Task.jsx
+++ client/src/Task.jsx
@@ -6,7 +6,13 @@ export default function Task() {
     <div className="flex max-w-lg gap-x-4">
       <Checkbox />
       <span className="grow">Do the dishes</span>
-      <button type="button">X</button>
+      <button type="button">
+        <svg width="35px" height="35px" version="1.1" viewBox="0 0 700 700">
+          <path d="m400.4 71.68h-100.8c-16.801 0-30.801 14-30.801 30.801v28h-95.199v39.199h28v221.2c0 47.602 39.199 86.801 86.801 86.801h123.2c47.602 0 86.801-39.199 86.801-86.801l-0.003906-221.76h28v-39.199h-95.199v-28c0-16.801-14-30.242-30.801-30.242zm-92.398 39.199h84v19.602h-84zm151.2 280c0 26.32-21.281 47.602-47.602 47.602h-123.2c-26.32 0-47.602-21.281-47.602-47.602l0.003906-221.76h218.4z" />
+        </svg>
+      </button>
     </div>
   );
 }
+
+Task.attributions = `Delete by Ananth from <a href="https://thenounproject.com/browse/icons/term/delete/" target="_blank" title="Delete Icons">Noun Project</a>`;
