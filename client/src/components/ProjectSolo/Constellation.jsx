1a528e11cf0d98edb4b147b21284b547758d5f40
Ellina Shcherbaeva <s.ellina2020@gmail.com>
2023-02-09 23:17:18 +1100

    Constellations have colours

--- client/src/components/ProjectSolo/Constellation.jsx
+++ client/src/components/ProjectSolo/Constellation.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Constellation({
   width,
@@ -7,27 +7,36 @@ export default function Constellation({
   horoscope,
   label,
 }) {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible((currentState) => {
+      return !currentState;
+    });
+  };
   const shortHoroscope = horoscope.split(".").slice(0, 2).join(".") + ".";
   return (
     <div
       className="relative group overflow-y-scroll constellation"
+      onClick={onClick}
       style={{
         width: `${width}px`,
         height: `${height}px`,
       }}
     >
       {children}
-      <div className="opacity-0 transition-opacity duration-300 text-sm constellation-content">
-        {/* <a
-          className="twitter-share-button"
-          href={`https://twitter.com/intent/tweet?text=${shortHoroscope}`}
-          data-size="large"
-        >
-          Tweet
-        </a> */}
-        <h3 className="mt-4 text-md font-bold">{label.toUpperCase()}</h3>
-        <p className="mt-4">{shortHoroscope}</p>
-      </div>
+      {visible ? (
+        <div className="transition-opacity duration-300 text-sm constellation-content">
+          <a
+            className="twitter-share-button"
+            href={`https://twitter.com/intent/tweet?text=${shortHoroscope}`}
+            data-size="large"
+          >
+            Tweet
+          </a>
+          <h3 className="mt-4 text-md font-bold">{label.toUpperCase()}</h3>
+          <p className="mt-4">{shortHoroscope}</p>
+        </div>
+      ) : null}
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,77 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  safelist: [
+    "group-hover:before:bg-teal-400",
+    "group-hover:before:bg-teal-600",
+    "group-hover:before:bg-teal-700",
+    "group-hover:after:bg-teal-400",
+    "group-hover:after:bg-teal-600",
+    "group-hover:after:bg-teal-700",
+
+    "group-hover:before:bg-purple-400",
+    "group-hover:before:bg-purple-600",
+    "group-hover:before:bg-purple-700",
+    "group-hover:after:bg-purple-400",
+    "group-hover:after:bg-purple-600",
+    "group-hover:after:bg-purple-700",
+
+    "group-hover:before:bg-cyan-400",
+    "group-hover:before:bg-cyan-600",
+    "group-hover:before:bg-cyan-700",
+    "group-hover:after:bg-cyan-400",
+    "group-hover:after:bg-cyan-600",
+    "group-hover:after:bg-cyan-700",
+
+    "group-hover:before:bg-sky-400",
+    "group-hover:before:bg-sky-600",
+    "group-hover:before:bg-sky-700",
+    "group-hover:after:bg-sky-400",
+    "group-hover:after:bg-sky-600",
+    "group-hover:after:bg-sky-700",
+
+    "group-hover:before:bg-fuchsia-400",
+    "group-hover:before:bg-fuchsia-600",
+    "group-hover:before:bg-fuchsia-700",
+    "group-hover:after:bg-fuchsia-400",
+    "group-hover:after:bg-fuchsia-600",
+    "group-hover:after:bg-fuchsia-700",
+
+    "group-hover:before:bg-indigo-400",
+    "group-hover:before:bg-indigo-600",
+    "group-hover:before:bg-indigo-700",
+    "group-hover:after:bg-indigo-400",
+    "group-hover:after:bg-indigo-600",
+    "group-hover:after:bg-indigo-700",
+
+    "group-hover:before:bg-violet-400",
+    "group-hover:before:bg-violet-600",
+    "group-hover:before:bg-violet-700",
+    "group-hover:after:bg-violet-400",
+    "group-hover:after:bg-violet-600",
+    "group-hover:after:bg-violet-700",
+
+    "group-hover:before:bg-green-400",
+    "group-hover:before:bg-green-600",
+    "group-hover:before:bg-green-700",
+    "group-hover:after:bg-green-400",
+    "group-hover:after:bg-green-600",
+    "group-hover:after:bg-green-700",
+
+    "group-hover:before:bg-lime-400",
+    "group-hover:before:bg-lime-600",
+    "group-hover:before:bg-lime-700",
+    "group-hover:after:bg-lime-400",
+    "group-hover:after:bg-lime-600",
+    "group-hover:after:bg-lime-700",
+
+    "group-hover:before:bg-blue-400",
+    "group-hover:before:bg-blue-600",
+    "group-hover:before:bg-blue-700",
+    "group-hover:after:bg-blue-400",
+    "group-hover:after:bg-blue-600",
+    "group-hover:after:bg-blue-700",
+  ],
   theme: {
     extend: {
       fontFamily: {
