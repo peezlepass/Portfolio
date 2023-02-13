878e1efaab7d684735ac9eba4d10cfa3e86b1deb
Ellina Shcherbaeva <s.ellina2020@gmail.com>
2023-02-13 22:17:51 +1100

    Minesweeper BombGuess Cell styling

--- client/src/components/Minesweeper/Cells/BombGuess.jsx
+++ client/src/components/Minesweeper/Cells/BombGuess.jsx
@@ -1,3 +1,10 @@
 export default function BombGuess() {
-  return <div>BombGuess</div>;
+  return (
+    <div
+      className="w-20 h-20 bg-empty-cell-color border-8 text-6xl text-center"
+      style={{ borderStyle: "outset" }}
+    >
+      🚩
+    </div>
+  );
 }
