762fd8410fd5208e8f804ef77dcd56e3416da4ea
Ellina Shcherbaeva <s.ellina2020@gmail.com>
2023-02-13 22:45:40 +1100

    Minesweeper Counter Numbers styling

--- client/src/components/Minesweeper/Counter.jsx
+++ client/src/components/Minesweeper/Counter.jsx
@@ -1,3 +1,27 @@
+import Zero from "./Counter/Zero";
+import One from "./Counter/One";
+import Two from "./Counter/Two";
+import Three from "./Counter/Three";
+import Four from "./Counter/Four";
+import Five from "./Counter/Five";
+import Six from "./Counter/Six";
+import Seven from "./Counter/Seven";
+import Eight from "./Counter/Eight";
+import Nine from "./Counter/Nine";
+
 export default function Counter() {
-  return <div>Counter</div>;
+  return (
+    <>
+      <Zero></Zero>
+      <One></One>
+      <Two></Two>
+      <Three></Three>
+      <Four></Four>
+      <Five></Five>
+      <Six></Six>
+      <Seven></Seven>
+      <Eight></Eight>
+      <Nine></Nine>
+    </>
+  );
 }
---
+++ client/src/components/Minesweeper/Counter/Three.jsx
@@ -0,0 +1,53 @@
+export default function Three() {
+  return (
+    <svg
+      width="40px"
+      height="80px"
+      style={{
+        shapeRendering: "geometricPrecision",
+        textRendering: "geometricPrecision",
+        imageRendering: "optimizeQuality",
+        fillRule: "evenodd",
+        clipRule: "evenodd",
+      }}
+      viewBox="0 0 4.50183 8.61276"
+    >
+      <rect fill="black" width="4.50183" height="8.61276" />
+      <polygon
+        id="A"
+        fill="red"
+        points="0.156546,0.000883515 1.39867,1.21768 3.11825,1.21761 4.34529,0 "
+      />
+      <polygon
+        id="D"
+        fill="red"
+        points="0.156546,8.61188 1.39867,7.39508 3.11825,7.39515 4.34529,8.61276 "
+      />
+      <polygon
+        id="C"
+        fill="red"
+        points="4.50095,8.49516 3.28415,7.25303 3.28422,5.53345 4.50183,4.30641 "
+      />
+      <polygon
+        id="B"
+        fill="red"
+        points="4.50095,4.30632 3.28415,3.06419 3.28422,1.34461 4.50183,0.117573 "
+      />
+      <polygon
+        id="E"
+        fill="#600000"
+        points="0.000883515,8.49516 1.21768,7.25303 1.21761,5.53345 0,4.30641 "
+      />
+      <polygon
+        id="F"
+        fill="#600000"
+        points="0.000883515,4.30632 1.21768,3.06419 1.21761,1.34461 0,0.117573 "
+      />
+      <polygon
+        id="G"
+        fill="red"
+        points="0.156546,4.27664 1.39867,4.98328 3.11825,4.98322 4.34519,4.27621 4.34529,4.27621 4.34522,4.27618 4.34529,4.27615 4.34519,4.27615 3.11825,3.56914 1.39867,3.56907 0.156546,4.27572 4.06603,4.27618 "
+      />
+    </svg>
+  );
+}
