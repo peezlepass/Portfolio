304e91cddd3157a3bcfe3f50b2b70d9bfa8008a5
Ellina Shcherbaeva <s.ellina2020@gmail.com>
2023-08-23 21:09:30 +1000

    Playing with fillRect

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const ctx = canvas.getContext("2d");
 
 ctx.fillStyle = "green";
 ctx.fillRect(10, 10, 150, 100);
+ctx.fillStyle = "red";
+ctx.fillRect(200, 200, 300, 150);
+ctx.fillStyle = "blue";
+ctx.fillRect(400, 400, 150, 300);
+ctx.fillStyle = "yellow";
+ctx.fillRect(600, 10, 150, 100);
