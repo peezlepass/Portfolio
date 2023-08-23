322f1df1168e47c42ee84530b81bc6db0d60ab6c
Ellina Shcherbaeva <s.ellina2020@gmail.com>
2023-08-23 21:46:59 +1000

    Running of colourful rectangles

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("simulator");
 
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "green";
+// ctx.fillStyle = "green";
 // ctx.fillRect(10, 10, 150, 100);
 // ctx.clearRect(10, 10, 140, 90);
 // ctx.fillStyle = "red";
@@ -13,7 +13,11 @@ ctx.fillStyle = "green";
 // ctx.fillRect(600, 10, 150, 100);
 
 let x = 10;
+let colour = ["red", "green", "blue"];
+let colourIndex = 0;
 setInterval(() => {
+  ctx.fillStyle = colour[colourIndex % colour.length];
   ctx.fillRect(x, 10, 150, 100);
   x += 5;
+  colourIndex += 1;
 }, 16);
