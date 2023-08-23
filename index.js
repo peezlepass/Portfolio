815bd84f336079e50bd0c401fee5a940c0383dee
Ellina Shcherbaeva <s.ellina2020@gmail.com>
2023-08-23 21:35:03 +1000

    Running green rectangle

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const canvas = document.getElementById("simulator");
 const ctx = canvas.getContext("2d");
 
 ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 150, 100);
-ctx.clearRect(10, 10, 140, 90);
-ctx.fillStyle = "red";
-ctx.fillRect(200, 200, 300, 150);
-ctx.fillStyle = "blue";
-ctx.fillRect(400, 400, 150, 300);
-ctx.fillStyle = "yellow";
-ctx.fillRect(600, 10, 150, 100);
+// ctx.fillRect(10, 10, 150, 100);
+// ctx.clearRect(10, 10, 140, 90);
+// ctx.fillStyle = "red";
+// ctx.fillRect(200, 200, 300, 150);
+// ctx.fillStyle = "blue";
+// ctx.fillRect(400, 400, 150, 300);
+// ctx.fillStyle = "yellow";
+// ctx.fillRect(600, 10, 150, 100);
+
+let x = 10;
+setInterval(() => {
+  ctx.fillRect(x, 10, 150, 100);
+  x += 5;
+}, 16);
